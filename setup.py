cb971371ff93b41c95138d7edc8c99ed7d762c89
Ap0c <Ap0c@users.noreply.github.com>
2016-03-07 20:24:38 +0000

    Added main package and setup.py with requirements.

---
+++ setup.py
@@ -0,0 +1,13 @@
+# ----- Imports ----- #
+
+from distutils.core import setup
+
+
+# ----- Setup ----- #
+
+setup(
+	name='blog-engine',
+	version='0.1.0',
+	author='Ap0c',
+	install_requires=['markdown', 'jinja2']
+)
