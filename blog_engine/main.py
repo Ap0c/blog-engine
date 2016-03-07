b3a8f84eee1b59b39a78e8a058f2d1df790973dc
Ap0c <Ap0c@users.noreply.github.com>
2016-03-07 21:00:28 +0000

    Set up setters and getters for the articles build directory.

--- blog_engine/main.py
+++ blog_engine/main.py
@@ -15,8 +15,26 @@ class Engine():
 	"""The main Engine object, used to generate the static site from source."""
 
 	_articles_dir = 'articles'
+	_articles_url = '/' + _articles_dir + '/'
 
 	def __init__(self, src='content', build='build'):
 
+		"""Sets up source and build directories."""
+
 		self._src = src
 		self._build = build
+
+	@property
+	def articles_dir(self):
+
+		"""Getter for the articles build directory."""
+
+		return self._articles_dir
+
+	@articles_dir.setter
+	def articles_dir(self, value):
+
+		"""Sets the name in the build directory for articles."""
+
+		self._articles_dir = value
+		self._articles_url = '/' + value + '/'
